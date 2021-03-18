a2a7ccf2e215d171b7fc68b0ea3ea8025b93e473
leoyassuda <@Noriyoshi07>
2021-03-18 17:16:26 -0300

    first commit

---
+++ src/main/java/com/lny/gateway/GatewayApplication.java
@@ -0,0 +1,35 @@
+package com.lny.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(routeSpec ->
+                        routeSpec
+                                .path("/hello")
+                                .filters(f -> f.addRequestHeader("Hello", "World"))
+                                .uri("http://httpbin.org")
+                )
+//                .route("twitter", routeSpec ->
+//                        routeSpec.path("/twitter/**")
+//                                .filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath(
+//                                        "/twitter/(?<handle>.*)",
+//                                        "/${handle}"))
+//                                .uri("http://twitter.com/@")
+//                )
+                .build();
+    }
+}
