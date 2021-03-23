764013338a5bef79e32eb55eb3767a5f34b6055b
Leo Yassuda <leo.yassuda@gmail.com>
2021-03-23 18:03:02 -0300

    add simple security for test and circuit breaker

--- src/main/java/com/lny/gateway/GatewayDiscoveryApplication.java
+++ src/main/java/com/lny/gateway/GatewayDiscoveryApplication.java
@@ -4,15 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.event.RefreshRoutesResultEvent;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.SetPathGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.CachingRouteLocator;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -22,37 +26,82 @@ public class GatewayDiscoveryApplication {
         SpringApplication.run(GatewayDiscoveryApplication.class, args);
     }
 
+//    @Bean
+//    RedisRateLimiter redisRateLimiter() {
+//        return new RedisRateLimiter(5, 10);
+//    }
+
+    @Bean
+    SecurityWebFilterChain authorization(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .httpBasic(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ae -> ae
+                        .pathMatchers("/hello").authenticated()
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    MapReactiveUserDetailsService authentication() {
+        return new MapReactiveUserDetailsService(User.withDefaultPasswordEncoder()
+                .username("leo")
+                .password("123")
+                .roles("USER")
+                .build());
+    }
+
     @Bean
     ApplicationListener<RefreshRoutesResultEvent> routesRefreshed() {
-        return new ApplicationListener<RefreshRoutesResultEvent>() {
-            @Override
-            public void onApplicationEvent(RefreshRoutesResultEvent refreshRoutesResultEvent) {
-                System.out.println("Routes updated");
-                var cachingRouteLocator = (CachingRouteLocator) refreshRoutesResultEvent.getSource();
-                Flux<Route> routes = cachingRouteLocator.getRoutes();
-                routes.subscribe(System.out::println);
-            }
+        return refreshRoutesResultEvent -> {
+            System.out.println("Routes updated");
+            var cachingRouteLocator = (CachingRouteLocator) refreshRoutesResultEvent.getSource();
+            Flux<Route> routes = cachingRouteLocator.getRoutes();
+            routes.subscribe(System.out::println);
         };
     }
 
+    //circuit-breaker
     @Bean
-    RouteLocator gateway(SetPathGatewayFilterFactory filterFactory) {
-        var singleRoute = Route.async()
-                .id("test-route")
-                .filter(new OrderedGatewayFilter(
-                        filterFactory.apply(config ->
-                                config.setTemplate("/products")), 1
-                ))
-                .uri("lb://product-service")
-                .asyncPredicate(serverWebExchange -> {
-                    var uri = serverWebExchange.getRequest().getURI();
-                    var path = uri.getPath();
-                    var match = path.contains("/products");
-                    return Mono.just(match);
-                })
+    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+
+                .route(routeSpec -> routeSpec
+                        .path("/hello")
+//                        .filters(filterSpec -> filterSpec
+//                                        .requestRateLimiter(requestRateLimit -> requestRateLimit
+//                                                        .setRateLimiter(redisRateLimiter())
+//                                        .setKeyResolver(new PrincipalNameKeyResolver())
+//                                        )
+//                        )
+                        .uri("lb://product-service"))
+
+                .route(routeSpec -> routeSpec
+                        .path("/default")
+                        .filters(filterSpec -> filterSpec
+                                .filter((exchange, chain) -> {
+                                    System.out.println("This is your second chance!");
+                                    System.out.println("URL: " + exchange.getRequest().getURI());
+                                    return chain.filter(exchange);
+                                }))
+                        .uri("https://spring.io/guides")
+                )
+                .route(routeSpec -> routeSpec
+                        .path("/products")
+                        .filters(filterSpec -> filterSpec
+                                .circuitBreaker(
+                                        config -> config.setFallbackUri("forward:/default")
+                                ))
+                        .uri("lb://product-service")
+                )
+                .route(routeSpec -> routeSpec
+                        .path("/error/**")
+                        .filters(filterSpec -> filterSpec.retry(5))
+                        .uri("lb://product-service")
+                )
                 .build();
-
-        return () -> Flux.just(singleRoute);
-
     }
+
 }
